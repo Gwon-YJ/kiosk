5d76f79df61a3ea1a19358ebf6edf799e760edf7
권용준 <yoyo99828@naver.com>
2025-04-30 19:26:06 +0900

    수정

--- src/kiosk4/Menu.java
+++ src/kiosk4/Menu.java
@@ -9,11 +9,11 @@ public class Menu {
     private List<MenuItem> items;
 
     // 2. 생성자
-
     public Menu(String menuName, List<MenuItem> items) {
         this.menuName = menuName;
         this.items = items;
     }
+
     // 3. 기능
     public String getMenuName() {
         return menuName;
@@ -21,6 +21,17 @@ public class Menu {
 
     public List<MenuItem> getItems() {
         return items;
+    }
 
+    public String getmenuBar() {
+        if (menuName.contains("Burgers")) {
+            return "BURGERS MENU";
+        } else if (menuName.contains("Drinks")) {
+            return "DRINK MENU";
+        } else if (menuName.contains("Desserts")) {
+            return "DESSERT MENU";
+        } else {
+            return menuName;
+        }
     }
 }
