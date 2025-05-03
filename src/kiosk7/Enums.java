a3eecbed92e8f43b33a692c1fa9bf544325d111a
권용준 <yoyo99828@naver.com>
2025-05-04 04:42:31 +0900

    Enum 클래스는 좀 다른 거 같다

--- src/kiosk7/Enums.java
+++ src/kiosk7/Enums.java
@@ -1,26 +1,28 @@
 package kiosk7;
 
 public class Enums {
-    //1. 속성
-    private double rate;
+    // 1. 속성
 
     // 2. 생성자
-    
+
     // 3. 기능
     public enum Menus {
         BURGERS, DRINKS, DESSERTS
     }
-    
+
     public enum Discount {
         VETERAN(0.10),   // 국가유공자 10%
         SOLDIER(0.05),   // 군인 5%
         STUDENT(0.03),   // 학생 3%
         GENERAL(0.00);   // 일반 0%
-        
-        Discount (double rate) {
+
+        private double rate;
+
+        Discount(double rate) {
             this.rate = rate;
         }
-        
+
+
         public double getRate() {
             return rate;
         }
