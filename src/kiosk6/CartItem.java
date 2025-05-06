3afe6fe6f326d86ea3cc7122ccc280210067ab9f
권용준 <yoyo99828@naver.com>
2025-05-06 10:29:07 +0900

    장바구니 아이템 클래스 추가

--- src/kiosk6/CartItem.java
+++ src/kiosk6/CartItem.java
@@ -1,34 +1,34 @@
 package kiosk6;
 
 public class CartItem {
-
+    
     // 1. 속성
     private MenuItem menuItem;
     private int quantity;
-
+    
     // 2. 생성자
     public CartItem(MenuItem menuItem, int quantity) {
         this.menuItem = menuItem;
         this.quantity = quantity;
     }
-
+    
     // 3. 기능
     public MenuItem getMenuItem() {
         return menuItem;
     }
-
+    
     public int getQuantity() {
         return quantity;
     }
-
+    
     public double getTotalPrice() {
         return menuItem.getPrice() * quantity;
     }
-
+    
     public void plus() {
         this.quantity++;
     }
-
+    
     @Override
     public String toString() {
         return menuItem.getName() + " | 수량: " + quantity + " | 가격: W " + getTotalPrice();
