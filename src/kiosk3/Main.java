372586cf2761615ad7f629d33eeffa178ff50e97
권용준 <yoyo99828@naver.com>
2025-05-06 13:53:45 +0900

    메인

--- src/kiosk3/Main.java
+++ src/kiosk3/Main.java
@@ -19,7 +19,7 @@ public class Main {
         menu.add(new MenuItem("Cheeseburger", 6.9, "포테이토 번과 비프패티, 치즈가 토핑된 치즈버거"));
         menu.add(new MenuItem("Hamburger", 5.4, "비프패티를 기반으로 야채가 들어간 기본 치즈버거"));
         
-        // 키오스크 실행
+        // Kiosk 실행
         Kiosk kiosk = new Kiosk(menu);
         kiosk.start();
     }
