a9618241ffda394e8103191fc2ad4b31a5d66953
권용준 <yoyo99828@naver.com>
2025-05-06 10:35:39 +0900

    Update Kiosk.java

--- src/kiosk7/Kiosk.java
+++ src/kiosk7/Kiosk.java
@@ -18,7 +18,7 @@ public class Kiosk {
         this.scanner = new Scanner(System.in);
     }
 
-    // 3. 기능 
+    // 3. 기능
     public void start() {
 
         while (true) {
@@ -27,7 +27,7 @@ public class Kiosk {
             for (int i = 0; i < menus.size(); i++) {
                 System.out.println((i + 1) + ". " + menus.get(i).getName());
             }
-            
+
             if (!items.isEmpty()) {
                 System.out.println("[ ORDER MENU ]");
                 System.out.println("4. Orders       | 장바구니를 확인 후 주문합니다.");
@@ -50,7 +50,86 @@ public class Kiosk {
                 order();    // order
             } else if (start == 5 && !items.isEmpty()) {
                 clearCart(); // clear
-            } else if (start ==트 
+            } else if (start == 0) {
+                System.out.println("프로그램을 종료합니다.");
+                break;
+            } else {
+                System.out.println("잘못된 번호입니다. 다시 입력해주세요");
+            }
+        }
+
+        scanner.close();
+    }
+
+    //뒤로가기
+    public void back(Menu menu) {
+
+
+        while (true) {
+            System.out.println("[ " + menu.getMenuBar() + " ]");
+
+            for (int i = 0; i < menu.getMenuMap().size(); i++) {
+                MenuItem item = menu.getMenuMap().get(i);
+                System.out.printf("%d. %-15s | W %-2.1f | %s\n",
+                        i + 1, item.getName(), item.getPrice(), item.getExplanation());
+            }
+
+            System.out.println("0. 뒤로 가기");
+
+            System.out.print("메뉴 번호 선택: ");
+            int back1 = scanner.nextInt();
+
+            if (back1 == 1) {
+                MenuItem menuItem = menu.getMenuMap().get(0);
+                System.out.println("1. " + menuItem.getName() + "  | W " + menuItem.getPrice() + " | " + menuItem.getExplanation());
+            } else if (back1 == 2) {
+                MenuItem menuItem = menu.getMenuMap().get(1);
+                System.out.println("2. " + menuItem.getName() + "  | W " + menuItem.getPrice() + " | " + menuItem.getExplanation());
+            } else if (back1 == 3) {
+                MenuItem menuItem = menu.getMenuMap().get(2);
+                System.out.println("3. " + menuItem.getName() + "  | W " + menuItem.getPrice() + " | " + menuItem.getExplanation());
+            } else if (back1 == 4) {
+                MenuItem menuItem = menu.getMenuMap().get(3);
+                System.out.println("4. " + menuItem.getName() + "  | W " + menuItem.getPrice() + " | " + menuItem.getExplanation());
+            } else if (back1 == 0) {
+                System.out.println("프로그램을 종료합니다.");
+                return;
+            } else {
+                System.out.println("잘못된 번호입니다. 다시 입력해주세요");
+            }
+
+            // 장바구니
+            MenuItem cart = menu.getMenuMap().get(back1 - 1);
+            System.out.printf("\n\"%s | W %.1f | %s\"\n", cart.getName(), cart.getPrice(), cart.getExplanation());
+            System.out.println("위 메뉴를 장바구니에 추가하시겠습니까?");
+            System.out.println("1. 확인        2. 취소");
+            System.out.print("선택: ");
+            int back2 = scanner.nextInt();
+
+            // 중복일 때 수량 증가하도록
+            if (back2 == 1) {
+                boolean isExist = false;
+
+                for (CartItem cartItem : items) {
+                    if (cartItem.getMenuItem().getName().equals(cart.getName())) {
+                        cartItem.plus();  // 수량만 증가
+                        isExist = true;
+                        break;
+                    }
+                }
+
+                if (!isExist) {
+                    items.add(new CartItem(cart, 1));  // 처음 추가할 때만 넣기
+                }
+
+                System.out.println(cart.getName() + " 이 장바구니에 추가되었습니다.\n");
+            } else {
+                System.out.println("추가되지 않았습니다.\n");
+                return;
+            }
+        }
+    }
+       // 할인 대상자 리스트
         public Enums.Discount customer() {
             System.out.println("\n[고객 유형 선택]");
             System.out.println("1. 국가유공자 (10% 할인)");
@@ -114,7 +193,7 @@ public class Kiosk {
             System.out.println("잘못된 숫자를 입력하였습니다.");
         }
     }
-    
+
     // 청소
     public void clearCart() {
         items.clear();
