2cd14f5d396031cfc16a23d1b6b05b73abd3c8e9
권용준 <yoyo99828@naver.com>
2025-05-06 10:31:07 +0900

    메인 수정

--- src/kiosk6/Main.java
+++ src/kiosk6/Main.java
@@ -10,18 +10,18 @@ public class Main {
 
     // 3. 기능
     public static void main(String[] args) {
-
+        
         // 각 메뉴 항목을 생성
         Menu burgerMenu = new Menu("Burgers");
         Menu drinkMenu = new Menu("Drinks");
         Menu dessertMenu = new Menu("Desserts");
-
+        
         // Kiosk에 메뉴 리스트 전달
         List<Menu> menus = new ArrayList<>();
         menus.add(burgerMenu);
         menus.add(drinkMenu);
         menus.add(dessertMenu);
-
+        
         // Kiosk 실행
         Kiosk kiosk = new Kiosk(menus);
         kiosk.start();
