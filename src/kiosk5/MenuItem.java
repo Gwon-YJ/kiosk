a6e419a8f15281c9bbf4ffc9f1565f80d5cb1d8d
권용준 <yoyo99828@naver.com>
2025-05-06 10:48:48 +0900

    캡슐화

--- src/kiosk5/MenuItem.java
+++ src/kiosk5/MenuItem.java
@@ -5,23 +5,23 @@ public class MenuItem {
     private String name;
     private double price;
     private String explanation;
-
+    
     // 2. 생성자
     public MenuItem(String name, double price, String explanation) {
         this.name = name;
         this.price = price;
         this.explanation = explanation;
     }
-
+    
     // 3. 기능
     public String getName() {
         return name;
     }
-
+    
     public double getPrice() {
         return price;
     }
-
+    
     public String getExplanation() {
         return explanation;
     }
