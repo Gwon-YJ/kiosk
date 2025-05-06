dd460523b76c014464953e38d53084dbc555cf0d
권용준 <yoyo99828@naver.com>
2025-05-06 13:28:38 +0900

    메인

--- src/kiosk2/Main.java
+++ src/kiosk2/Main.java
@@ -11,14 +11,16 @@ public class Main {
     
     // 3. 기능
     public static void main(String[] args) {
+        // 리스트 추가
         List<MenuItem> menuItems = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
-        
+
+        // 햄버거 메뉴 리스트
         menuItems.add(new MenuItem("ShackBurger", 6.9, "토마토, 양상추, 쉑소스가 토핑된 치츠버거"));
         menuItems.add(new MenuItem("SmokeShack", 8.9, "베이컨, 체리, 페퍼에 쉑소스가 포팅된 치즈버거"));
         menuItems.add(new MenuItem("Cheeseburger", 6.9, "포테이토 번과 비프패티, 치즈가 토핑된 치즈버거"));
         menuItems.add(new MenuItem("Hamburger", 5.4, "비프패티를 기반으로 야채가 들어간 기본 치즈버거"));
-        
+
         while (true) {
             System.out.println("[ SHAKESHACK MENU ]");
             System.out.println("1. ShackBurger   | W 6.9 | 토마토, 양상추, 쉑소스가 토핑된 치즈버거");
@@ -27,9 +29,11 @@ public class Main {
             System.out.println("4. Hamburger     | W 5.4 | 비프패티를 기반으로 야채가 들어간 기본버거");
             System.out.println("0. 종료");
 
+            // 입력
             System.out.println("메뉴 번호를 선택");
             int menu = scanner.nextInt();
 
+            // 출력
             if (menu == 1) {
                 MenuItem menuItem = menuItems.get(0);
                 System.out.println("1. " + menuItem.getName() + "  | W " + menuItem.getPrice() + " | " + menuItem.getExplanation());
